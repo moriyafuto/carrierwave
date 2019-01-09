941be0a1e9a44f2c32b4eebe7b480722b8690382
“moriya” <kaede0606f@icloud.com>
2019-01-09 06:52:18 +0000

    first commit

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,16 @@
+class PhotosController < ApplicationController
+    
+    def new
+        @photo = Photo.new
+    end
+    
+    def create
+        @photo = Photo.create(params.require(:photo).permit(:image))
+        redirect_to("/photos/#{@photo.id}")
+    end
+    
+    def show
+        @photo = Photo.find(id: params[:id])
+    end
+    
+end
